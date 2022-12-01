e088d1d874d3494f71b5be4acc1820135ebd92f7
unknown <akhand04decgmail.com>
2022-12-01 07:20:10 +0530

    added code

---
+++ code/Day 1/calculator.py
@@ -0,0 +1,16 @@
+# Creating a Calculator
+import sys
+
+list1 = sys.argv
+
+# Adding the values
+# Sub the values
+# Divid
+# Multiplicaion
+
+# basic need is to add numbers we need atleast 2 number
+
+# Adding Functionality
+print("First Number =",int(list1[1]))
+print("Second Number =",int(list1[2]))
+print("Sum = ",int(list1[1])+int(list1[2]))
